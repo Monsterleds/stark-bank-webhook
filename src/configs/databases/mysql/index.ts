36c327bc910a91aab229cec0e008aa3efd7ccc8e
Khaled Mohamad <khaled@contemplato.com>
2024-07-10 00:27:04 -0300

    feature: new enviroment

--- src/configs/databases/mysql/index.ts
+++ src/configs/databases/mysql/index.ts
@@ -4,7 +4,7 @@ import { Invoice } from "../../../models/invoice.model";
 
 const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.MYSQL_DATABASE_HOST,
     port: 3306,
     username: process.env.MYSQL_DATABASE_USER,
     password: process.env.MYSQL_DATABASE_ROOT_PASSWORD,
